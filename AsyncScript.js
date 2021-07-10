b42b6df900873e5ef255b33640954135a4194377
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-07-11 04:33:33 +1000

    feat: introducing the asynchronous unit test

--- AsyncScript.js
+++ AsyncScript.js
@@ -1,15 +1,28 @@
 const fetch = require('node-fetch');
 
+const getPeoplePromise = (fetch) => {
+  return fetch('https://swapi.dev/api/people')
+    .then(Response => Response.json())
+    .then(data => {
+      
+      return {
+        count: data.count,
+        results: data.results
+      }
+    })     
+}
+
 const getPeople = async (fetch) => {
   const getRequest = await fetch('https://swapi.dev/api/people');
   const data = await getRequest.json();
 
-  console.log(data);
-  
   return {
     count: data.count,
     results: data.results
   } 
 }
 
-getPeople(fetch);
+module.exports = {
+  getPeople,
+  getPeoplePromise
+}
